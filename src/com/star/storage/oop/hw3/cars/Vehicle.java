2217fbcb00b8c74352e0cb32009955be7232cc97
Theodor Negrescu <theodor.negrescu@gmail.com>
2021-05-08 12:33:31 +0300

    Add abstract vehicle class

---
+++ src/com/star/storage/oop/hw3/cars/Vehicle.java
@@ -0,0 +1,34 @@
+package com.star.storage.oop.hw3.cars;
+
+import static java.lang.Math.*;
+
+public abstract class Vehicle{
+	protected double speed;//mps
+	protected double angle;//degrees
+	protected double x, y;//meters
+
+	public double getSpeed(){
+		return speed;
+	}
+
+	public double getAngle(){
+		return angle;
+	}
+
+	public double getX(){
+		return x;
+	}
+
+	public double getY(){
+		return y;
+	}
+
+	//assumes constant velocity and angle
+	//public move method should call this
+	//acceleration methods should call this with average velocity and time taken to reach target velocity
+	protected void move(double seconds, double speed, double angle){
+		double r_angle = toRadians(angle);
+		x += speed * seconds * cos(r_angle);
+		y += speed * seconds * sin(r_angle);
+	}
+}
