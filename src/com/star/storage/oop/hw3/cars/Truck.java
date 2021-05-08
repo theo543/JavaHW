215c3f7c9aa77a21b1ab39ee58e23c12480792fb
Theodor Negrescu <theodor.negrescu@gmail.com>
2021-05-08 14:43:22 +0300

    Add Truck

---
+++ src/com/star/storage/oop/hw3/cars/Truck.java
@@ -0,0 +1,30 @@
+package com.star.storage.oop.hw3.cars;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class Truck extends Car{
+
+	private double percentLoaded;
+
+	public Truck(double angle, double x, double y, double percentLoaded){
+		super(angle, x, y);
+		setPercentLoaded(percentLoaded);
+	}
+
+	public double getPercentLoaded(){
+		return percentLoaded;
+	}
+
+	public void setPercentLoaded(double percentLoaded){
+		this.percentLoaded = min(1, max(percentLoaded, 0));
+	}
+
+	public double getAcceleration(){
+		return super.getAcceleration() * (1 - 0.2 * percentLoaded);//loaded truck accelerates 20% slower
+	}
+
+	public double getDeceleration(){
+		return super.getAcceleration() * (1 + 0.2 * percentLoaded);//loaded truck stops 20% faster
+	}
+}
