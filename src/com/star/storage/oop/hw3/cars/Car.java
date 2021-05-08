70eb5671814b34d818a5e49eac6af9e2c9495ae4
Theodor Negrescu <theodor.negrescu@gmail.com>
2021-05-08 14:08:16 +0300

    Add Car

---
+++ src/com/star/storage/oop/hw3/cars/Car.java
@@ -0,0 +1,61 @@
+package com.star.storage.oop.hw3.cars;
+
+import static java.lang.Math.*;
+
+public class Car extends Vehicle{
+
+	public double getAcceleration(){
+		return 3.5;//mps^-2
+	}
+
+	public double getDeceleration(){
+		return 7;//mps^-2
+	}
+
+	public double getSteering(){
+		return 40;//degrees per second
+	}
+
+	public void move(double seconds){
+		move(seconds, speed, angle);
+	}
+
+	//returns current speed
+	public double changeSpeed(double targetSpeed){
+		targetSpeed = min(getMaxSpeed(), targetSpeed);
+		targetSpeed = max(targetSpeed, 0);
+		double dv = (targetSpeed > speed) ? getAcceleration() : getDeceleration();
+		move(abs(targetSpeed - speed)/dv, (targetSpeed + speed)/2, angle);
+		speed = targetSpeed;
+		return speed;
+	}
+
+	//returns current angle
+	public double steer(double targetAngle, boolean right){
+		if(speed == 0)
+			return angle;
+		targetAngle %= 360;
+		if(targetAngle < 0)
+			targetAngle += 360;
+		double tick = 0.00001;//100000 simulations per second
+		double angleChange = getSteering() * tick * (right ? 1 : -1);
+		while(abs(targetAngle - angle) > angleChange){
+			move(tick, speed, angle);
+			angle += angleChange;
+		}
+		angle = targetAngle;
+		return angle;
+	}
+
+	public double getMaxSpeed(){
+		return 100;//mps
+	}
+
+	//initial angle and position
+	//angle of 0 degrees is east
+	public Car(double angle, double x, double y){
+		this.angle = angle;
+		this.x = x;
+		this.y = y;
+	}
+}
