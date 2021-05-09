9091c1fea10d45a755490cc19338a700964da384
theo543 <81699222+theo543@users.noreply.github.com>
2021-05-09 14:14:46 +0300

    Re-add AssertionError flag

--- src/com/star/storage/oop/AssertProvider.java
+++ src/com/star/storage/oop/AssertProvider.java
@@ -1,9 +1,20 @@
 package com.star.storage.oop;
 
 public class AssertProvider{
+	public static boolean getThrowsError(){
+		return throwsError;
+	}
+
+	public static void setThrowsError(boolean throwsError){
+		AssertProvider.throwsError = throwsError;
+	}
+
+	private static boolean throwsError = false;
 	public static void Assert(boolean b){
 		if(!b){
 			System.out.println("Assert failed");
+			if(throwsError)
+				throw(new AssertionError());
 		}
 	}
 }
