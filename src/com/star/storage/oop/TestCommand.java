76ad72c54f6c4da41b70d1be2f7736f2664429b6
theo543 <81699222+theo543@users.noreply.github.com>
2021-05-09 14:53:19 +0300

    Fix infinite loop
    Added TestWrapper class, "all" only calls that

--- src/com/star/storage/oop/TestCommand.java
+++ src/com/star/storage/oop/TestCommand.java
@@ -7,23 +7,24 @@ import com.star.storage.oop.hw1.Wall;
 import com.star.storage.oop.hw3.shapes.Cuboid;
 import com.star.storage.oop.hw3.shapes.Rectangle;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.function.Consumer;
 
 public class TestCommand{
-	private CommandParser parser = new CommandParser();
-	public void addTest(String name, Runnable r){
-		parser.add(new CommandParser.Command(name, (a)->r.run()));
-	}
+	private final CommandParser parser = new CommandParser();
+
 	public TestCommand(){
-		parser.add(new CommandParser.Command("toggle-assertion-error", (a)->{
+		parser.add(new CommandParser.Command("toggle-assertion-error", (a) -> {
 			if(a.matches("(?:true|false) ?"))
 				AssertProvider.setThrowsError(a.startsWith("t"));
 			else AssertProvider.setThrowsError(!AssertProvider.getThrowsError());
 			System.out.println("Assert errors are now " + (AssertProvider.getThrowsError() ? "on" : "off"));
 		}));
-		addTest("all", ()->parser.getCommands().forEach((c)->c.command().accept("")));
+		parser.add(new CommandParser.Command("all", (a) -> {
+			for(var t : parser.getCommands()){
+				if(t.command() instanceof TestWrapper)//avoid infinite loop
+					t.command().accept("");
+			}
+		}));
 		addTest("person", Person::testPerson);
 		addTest("complex-number", ComplexNumber::testComplexNumber);
 		addTest("point", Point::testPoint);
@@ -31,6 +32,22 @@ public class TestCommand{
 		addTest("cuboid", Cuboid::testCuboid);
 		addTest("rectangle", Rectangle::testRectangle);
 	}
+
+	private static class TestWrapper implements Consumer<String>{
+		Runnable r;
+		public TestWrapper(Runnable r){
+			this.r = r;
+		}
+		public void accept(String args){
+			r.run();
+			System.out.println("test done");
+		}
+	}
+
+	public void addTest(String name, Runnable r){
+		parser.add(new CommandParser.Command(name, new TestWrapper(r)));
+	}
+
 	public Consumer<String> getCommand(){
 		return parser::parse;
 	}
