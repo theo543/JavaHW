b38a4892b913fa3e7385b7ad07110cd42d6937c7
theo543 <81699222+theo543@users.noreply.github.com>
2021-05-11 14:35:53 +0300

    Add video classes

---
+++ src/com/star/storage/oop/hw4/Video.java
@@ -0,0 +1,44 @@
+package com.star.storage.oop.hw4;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class Video{
+	private static final DecimalFormat df = new DecimalFormat("#.##");
+	static{
+		df.setRoundingMode(RoundingMode.FLOOR);
+	}
+	private final String title;
+	private boolean checkedOut = false;
+	private double rating = 5;
+	private int ratings = 0;
+
+	public Video(String title){
+		this.title = title;
+	}
+
+	public boolean isCheckedOut(){
+		return checkedOut;
+	}
+
+	public void setCheckedOut(boolean checkedOut){
+		this.checkedOut = checkedOut;
+	}
+
+	public String getTitle(){
+		return title;
+	}
+
+	public double getRating(){
+		return rating;
+	}
+
+	public void updateRating(double d){
+		rating = (ratings * rating + Math.max(1, Math.min(10, d))) / (ratings + 1);
+		ratings++;
+	}
+
+	public String toString(){
+		return title + "(" + df.format(rating) + "/10)" + (checkedOut ? " - UNAVAILABLE" : " - AVAILABLE");
+	}
+}
---
+++ src/com/star/storage/oop/hw4/VideoStoreLauncher.java
@@ -0,0 +1,33 @@
+package com.star.storage.oop.hw4;
+
+import java.util.Random;
+
+public class VideoStoreLauncher{
+	public static void main(String[] args){
+		VideoStore vs = new VideoStore();
+		String[] list = new String[]{"The Matrix", "Godfather II", "Star Wars Episode IV: A New Hope"};
+		for(String v : list){
+			vs.addVideo(v);
+		}
+		var r = new Random();
+		for(int x = 0;x < 1000;x++)
+			r.nextInt();
+		for(int x = 0;x <= 50;x++){
+			vs.receiveRating(list[r.nextInt(3)], r.nextInt(3) + 8);
+		}
+		for(int x = 0;x <= 10;x++){
+			vs.receiveRating(list[r.nextInt(3)], r.nextInt(5));
+		}
+		vs.receiveRating(list[r.nextInt(3)], Integer.MIN_VALUE);
+		vs.receiveRating(list[r.nextInt(3)], Integer.MAX_VALUE);
+		for(String v : list){
+			vs.checkOut(v);
+			vs.returnVideo(v);
+		}
+		vs.checkOut("Godfather II");
+		vs.checkOut("TEST 123");
+		vs.returnVideo("TEST 321");
+		vs.receiveRating("TEST 213", Integer.MAX_VALUE);
+		vs.listVideos();
+	}
+}
