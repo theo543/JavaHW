1d3060f124837213c58173e28f57cb2953851402
theo543 <81699222+theo543@users.noreply.github.com>
2021-06-01 10:26:26 +0300

    Add VehicleCommand class

--- src/com/star/storage/oop/Main.java
+++ src/com/star/storage/oop/Main.java
@@ -1,6 +1,7 @@
 package com.star.storage.oop;
 
 import com.star.storage.oop.hw2.burgers.BurgerCommand;
+import com.star.storage.oop.hw3.cars.VehicleCommand;
 
 import java.util.Scanner;
 
@@ -11,9 +12,11 @@ public class Main {
     public static void main(String[] args) {
         CommandParser cli = new CommandParser();
         BurgerCommand burgerCli = new BurgerCommand();
+        VehicleCommand carCli = new VehicleCommand();
         cli.add("exit", (a) -> exitLoop = true);
         cli.add("test", TestCommand::parseTests);
         cli.add("burger", burgerCli::parse);
+        cli.add("car", carCli::parse);
         String input;
         Scanner s = new Scanner(System.in);
         System.out.println("Enter a command:");
---
+++ src/com/star/storage/oop/hw3/cars/VehicleCommand.java
@@ -0,0 +1,95 @@
+package com.star.storage.oop.hw3.cars;
+
+import com.star.storage.oop.CommandParser;
+
+import java.util.function.Consumer;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class VehicleCommand {
+    //TODO:add path display commands
+    private final CommandParser parser = new CommandParser();
+    private Car vehicle;
+
+    public VehicleCommand() {
+        parser.add("new-car", this::newCar);
+        parser.add("new-truck", this::newTruck);
+        addSafely("status", this::status);
+        addSafely("set-speed", this::setSpeed);
+        addSafely("move", this::move);
+        addSafely("set-loaded", this::setLoaded);
+        addSafely("steer-left", this::steerLeft);
+        addSafely("steer-right", this::steerRight);
+    }
+
+    private void steerLeft(String[] args) {
+        vehicle.steer(Double.parseDouble(args[0]), false);
+    }
+
+    private void steerRight(String[] args) {
+        vehicle.steer(Double.parseDouble(args[0]), true);
+    }
+
+    private void status(String[] args) {
+        System.out.println("x = " + vehicle.getX() + "m");
+        System.out.println("y = " + vehicle.getY() + "m");
+        System.out.println("angle = " + vehicle.getAngle() + "°");
+        System.out.println("speed = " + vehicle.getSpeed() + " mps");
+        if (vehicle instanceof Truck)
+            System.out.println("truck load = " + ((Truck) vehicle).getPercentLoaded()*100.0 + "%");
+    }
+
+    private void setLoaded(String[] args) {
+        if (!(vehicle instanceof Truck)) {
+            System.out.println("Vehicle is not a truck");
+        } else {
+            ((Truck) vehicle).setPercentLoaded(Double.parseDouble(args[0]));
+        }
+    }
+
+    private void setSpeed(String[] args) {
+        vehicle.changeSpeed(Double.parseDouble(args[0]));
+    }
+
+    private void move(String[] args) {
+        vehicle.move(Double.parseDouble(args[0]));
+    }
+
+    private void newCar(String[] args) {
+        if (args.length == 0) {
+            vehicle = new Car(0, 0, 0);
+            return;
+        }
+        double a, x, y;
+        a = Double.parseDouble(args[0]);
+        x = Double.parseDouble(args[1]);
+        y = Double.parseDouble(args[2]);
+        vehicle = new Car(a, x, y);
+    }
+
+    private void newTruck(String[] args) {
+        if (args.length == 0) {
+            vehicle = new Truck(0, 0, 0, 0);
+            return;
+        }
+        double a, x, y, l;
+        a = Double.parseDouble(args[0]);
+        x = Double.parseDouble(args[1]);
+        y = Double.parseDouble(args[2]);
+        l = min(1, max(0, Double.parseDouble(args[3])));
+        vehicle = new Truck(a, x, y, l);
+    }
+
+    public void addSafely(String name, Consumer<String[]> command) {
+        parser.add(name, (args) -> {
+            if (vehicle == null)
+                System.out.println("Vehicle not created yet");
+            else command.accept(args);
+        });
+    }
+
+    public void parse(String[] args) {
+        parser.parse(String.join(" ", args));
+    }
+}
