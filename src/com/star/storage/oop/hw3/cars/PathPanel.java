effdb499b5987751a8d83ab9315ce7a04caa6324
theo543 <81699222+theo543@users.noreply.github.com>
2021-06-04 12:25:54 +0300

    Save shapes to list so they're only calculated once
    Avoids doing all that trigonometry every time

--- src/com/star/storage/oop/hw3/cars/PathPanel.java
+++ src/com/star/storage/oop/hw3/cars/PathPanel.java
@@ -6,12 +6,14 @@ import java.awt.geom.Arc2D;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.Random;
 
 import static java.lang.Math.*;
 
 public class PathPanel extends JPanel {
     private final Car car;
+    private final ArrayList<Shape> shapes = new ArrayList<>();
 
     public PathPanel(Car car) {
         this.car = car;
@@ -40,7 +42,8 @@ public class PathPanel extends JPanel {
         g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         var prevTransform = g.getTransform();
         double squareLen = 100;
-        for (var m : car.getMovements()) {
+        var movements = car.getMovements();
+        for (var m : movements) {
             squareLen = max(max(abs(m.x), abs(m.y)), squareLen);
         }
         double x = car.getXStart(), y = car.getYStart();
@@ -51,42 +54,46 @@ public class PathPanel extends JPanel {
         g.draw(new Line2D.Double(0, -squareLen, 0, squareLen));
         g.draw(new Line2D.Double(squareLen, 0, -squareLen, 0));
         Random random = new Random(car.hashCode());
-        for (var m : car.getMovements()) {
+        for (int i = 0; i < movements.size(); i++) {
+            var m = movements.get(i);
             g.setColor(new Color(abs(random.nextInt() % 255), abs(random.nextInt() % 255), abs(random.nextInt() % 255)));
-            if (!m.isArc) {
-                g.draw(new Line2D.Double(x, y, m.x, m.y));
-            } else {
-                Point2D.Double to = new Point2D.Double(m.x, m.y);
-                Point2D.Double middle = new Point2D.Double((x + to.x) / 2, (y + to.y) / 2);
-                Point2D.Double c;
-                Arc2D arc = new Arc2D.Double();
-                double chord = to.distance(x, y), chordAngle, height, radius, hA, arcStart, arcExtent;
-                double arcAngle = m.arcAngle % (2 * PI);
-                if (arcAngle < PI) {
-                    chordAngle = arcAngle;
-                    radius = chord * sin((PI - chordAngle) / 2) / sin(chordAngle);
-                    height = sqrt(pow(radius, 2) - pow(middle.distance(to), 2));
-                    hA = atan2(middle.y - y, middle.x - x) + PI / 2;
-                    c = new Point2D.Double(middle.x + cos(hA) * height * (m.right ? 1 : -1), middle.y + sin(hA) * height * (m.right ? 1 : -1));
-                } else if (arcAngle > PI) {
-                    chordAngle = arcAngle - PI;
-                    radius = chord * sin((2 * PI - chordAngle) / 2) / sin(chordAngle);
-                    height = sqrt(pow(radius, 2) - pow(middle.distance(to), 2));
-                    hA = atan2(middle.y - y, middle.x - x) - PI / 2;
-                    c = new Point2D.Double(middle.x + cos(hA) * height * (m.right ? 1 : -1), middle.y + sin(hA) * height * (m.right ? 1 : -1));
+            if (shapes.size() <= i) {
+                if (!m.isArc) {
+                    shapes.add(new Line2D.Double(x, y, m.x, m.y));
                 } else {
-                    c = middle;
-                    radius = chord / 2;
-                }
-                if (m.arcAngle >= 2 * PI) {
-                    g.draw(new Ellipse2D.Double(c.x - radius, c.y - radius, 2 * radius, 2 * radius));
-                } else {
-                    arcStart = toDegrees(atan2(c.y - y, x - c.x));
-                    arcExtent = toDegrees(arcAngle) * (m.right ? -1 : 1);
-                    arc.setArcByCenter(c.x, c.y, radius, arcStart, arcExtent, Arc2D.OPEN); //for some reason these angles are anticlockwise?
-                    g.draw(arc);
+                    Point2D.Double to = new Point2D.Double(m.x, m.y);
+                    Point2D.Double middle = new Point2D.Double((x + to.x) / 2, (y + to.y) / 2);
+                    Point2D.Double c;
+                    Arc2D arc = new Arc2D.Double();
+                    double chord = to.distance(x, y), chordAngle, height, radius, hA, arcStart, arcExtent;
+                    double arcAngle = m.arcAngle % (2 * PI);
+                    if (arcAngle < PI) {
+                        chordAngle = arcAngle;
+                        radius = chord * sin((PI - chordAngle) / 2) / sin(chordAngle);
+                        height = sqrt(pow(radius, 2) - pow(middle.distance(to), 2));
+                        hA = atan2(middle.y - y, middle.x - x) + PI / 2;
+                        c = new Point2D.Double(middle.x + cos(hA) * height * (m.right ? 1 : -1), middle.y + sin(hA) * height * (m.right ? 1 : -1));
+                    } else if (arcAngle > PI) {
+                        chordAngle = arcAngle - PI;
+                        radius = chord * sin((2 * PI - chordAngle) / 2) / sin(chordAngle);
+                        height = sqrt(pow(radius, 2) - pow(middle.distance(to), 2));
+                        hA = atan2(middle.y - y, middle.x - x) - PI / 2;
+                        c = new Point2D.Double(middle.x + cos(hA) * height * (m.right ? 1 : -1), middle.y + sin(hA) * height * (m.right ? 1 : -1));
+                    } else {
+                        c = middle;
+                        radius = chord / 2;
+                    }
+                    if (m.arcAngle >= 2 * PI) {
+                        shapes.add(new Ellipse2D.Double(c.x - radius, c.y - radius, 2 * radius, 2 * radius));
+                    } else {
+                        arcStart = toDegrees(atan2(c.y - y, x - c.x));
+                        arcExtent = toDegrees(arcAngle) * (m.right ? -1 : 1);
+                        arc.setArcByCenter(c.x, c.y, radius, arcStart, arcExtent, Arc2D.OPEN); //for some reason these angles are anticlockwise?
+                        shapes.add(arc);
+                    }
                 }
             }
+            g.draw(shapes.get(i));
             x = m.x;
             y = m.y;
             var prev = g.getTransform();
